b3751b9ea2277b21b99c06fbef5cc2bfbd4359f2
Asepsa15 <asepsaripudin2002@gmail.com>
2023-03-09 20:50:09 +0700

    Tugas-praktek

---
+++ object-praktek.js
@@ -0,0 +1,54 @@
+// Objek 1
+const mobil = {
+    merek: "Toyota",
+    model: "Camry",
+    tahun: '2021',
+    warna: "Putih",
+    pintu: '4',
+  };
+  
+  // Outputkan semua data objek 1
+  console.log('Hasil Dari Object Satu');
+  console.log("Merek: ", mobil.merek);
+  console.log("Model: ", mobil.model);
+  console.log("Tahun: ", mobil.tahun);
+  console.log("Warna: ", mobil.warna);
+  console.log("Jumlah Pintu: ", mobil.pintu);
+  console.log('====================================');
+  
+  // Objek 2
+const buku = {
+    judul: "Harry Potter and The Philosopher's Stone",
+    penulis: "J.K. Rowling",
+    tahunTerbit: '1997',
+    penerbit: "Bloomsbury",
+    halaman: '223',
+  };
+  
+  // Outputkan semua data objek 2
+  console.log('Hasil Dari Object Dua');
+  console.log("Judul: ", buku.judul);
+  console.log("Penulis: ", buku.penulis);
+  console.log("Tahun Terbit: ", buku.tahunTerbit);
+  console.log("Penerbit: ", buku.penerbit);
+  console.log("Jumlah Halaman: ", buku.halaman);
+  console.log('====================================');
+  
+  // Objek 3
+const handphone = {
+    merek: "Samsung",
+    model: "Galaxy S21 Ultra",
+    tahun: '2021',
+    warna: "Hitam",
+    kapasitasBaterai: "5000 mAh"
+  };
+  
+  // Outputkan semua data objek 3
+  console.log('Hasil Dari Object Tiga');
+  console.log("Merek: ", handphone.merek);
+  console.log("Model: ", handphone.model);
+  console.log("Tahun: ", handphone.tahun);
+  console.log("Warna: ", handphone.warna);
+  console.log("Kapasitas Baterai: ", handphone.kapasitasBaterai);
+  console.log('====================================');
+  
